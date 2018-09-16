f386064928be73981c1a457c75e5e3b31f915254
Leonard Chen <wxclx98@qq.com>
2018-09-16 20:41:38 +0800

    Convert to MVC design patten.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Leonard Chen on 9/16/18.
+//  Copyright © 2018 Leonard Chan. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
